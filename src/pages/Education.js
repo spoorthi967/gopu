0a165134ba1528dae8058aaab62e42a5aa6ca0bc
spoorthi967 <gopuspoorthi18@gmail.com>
2025-03-03 12:49:45 -0500

    .

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -6,7 +6,6 @@ import "./Education.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-
 const Education = () => {
   const settings = {
     dots: true,
@@ -16,31 +15,19 @@ const Education = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-    arrows: true,
+    arrows: true, // Default slick arrows will be used
   };
 
   return (
-    
     <div className="education-main-container">
-
-
-<button class="slick-prev">
- <i class="bi bi-arrow-left-circle-fill"></i>
-</button>
-     
-
-      {/* Right Section - Sliding Education Content */}
       <div className="education-content">
         <h1 className="education-title">
           <i className="bi bi-mortarboard"></i> My Education
         </h1>
 
         <Slider {...settings}>
-          {/* Master's Degree */}
           <div className="education-card">
-            <h2>
-              <i className="bi bi-mortarboard"></i> Master of Science in Information Systems
-            </h2>
+            <h2><i className="bi bi-mortarboard"></i> Master of Science in Information Systems</h2>
             <p className="education-institute">
               <i className="bi bi-building"></i> Indiana Institute of Technology, Fort Wayne
             </p>
@@ -52,11 +39,8 @@ const Education = () => {
             </p>
           </div>
 
-          {/* Bachelor's Degree */}
           <div className="education-card">
-            <h2>
-              <i className="bi bi-mortarboard"></i> Bachelor of Technology in Electronics and Communication Engineering
-            </h2>
+            <h2><i className="bi bi-mortarboard"></i> Bachelor of Technology in Electronics and Communication Engineering</h2>
             <p className="education-institute">
               <i className="bi bi-building"></i> Swami Vivekananda Institute of Technology
             </p>
@@ -68,12 +52,6 @@ const Education = () => {
             </p>
           </div>
         </Slider>
-       
-        <button class="slick-next">
- <i class="bi bi-arrow-right-circle-fill"></i>
-</button>
-
-
       </div>
     </div>
   );
