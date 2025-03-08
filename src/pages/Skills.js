7b2f4c670b342f7beafed2232efe54a11a309c70
spoorthi967 <gopuspoorthi18@gmail.com>
2025-03-08 17:57:03 -0500

    .

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -4,12 +4,24 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Skills.css"; // Import styles
 
+import { 
+  FaReact, 
+  FaNodeJs, 
+  FaDatabase, 
+  FaGithub, 
+  FaTools, 
+  FaCode, 
+  FaBug, 
+  FaServer,
+  FaAngular,
+  FaVuejs,
+  FaCloud,
+  FaCodeBranch,
+  FaTasks
+} from "react-icons/fa";
 
-import { FaReact, FaNodeJs, FaDatabase, FaGithub, FaTools, FaCode, FaBug, FaServer } from "react-icons/fa";
 import { SiMongodb, SiGraphql, SiFirebase, SiPostgresql, SiMysql, SiRedis, SiSocketdotio } from "react-icons/si";
 import { FaAws } from "react-icons/fa6";
-
-
 
 
 
@@ -28,19 +40,22 @@ const skillsData = [
   {
     category: "Front-End Technologies",
     skills: [
-      { name: "React.js", icon: <FaReact /> },
-      { name: "Redux", icon: <FaReact /> },
-      { name: "JavaScript", icon: <FaCode /> },
-      {name: "Bootsrap" , icon: <FaCode/>},
-      {name: "Tailwind" , icon: <FaCode/>},
-      {name: "material ui" , icon: <FaCode/>},
-      {name: "React hooks" , icon: <FaReact/>},
-      {name: "typescript" , icon: <FaCode/>},
       { name: "HTML", icon: <FaCode /> },
       { name: "CSS", icon: <FaCode /> },
+      { name: "JavaScript", icon: <FaCode /> },
+      { name: "TypeScript", icon: <FaCode /> },
+      { name: "React.js", icon: <FaReact /> },
+      { name: "Redux", icon: <FaReact /> },
+      { name: "React Hooks", icon: <FaReact /> },
+      { name: "Vue.js", icon: <FaVuejs /> },
+      { name: "Next.js", icon: <FaCode /> },
+      { name: "Bootstrap", icon: <FaCode /> },
+      { name: "Tailwind CSS", icon: <FaCode /> },
+      { name: "Material UI", icon: <FaCode /> },
       { name: "API Integration", icon: <FaServer /> },
     ],
   },
+  
 
   {
     category: "Back-End Technologies",
@@ -70,9 +85,39 @@ const skillsData = [
       { name: "UI/UX Testing", icon: <FaBug /> },
       { name: "MongoDB", icon: <FaDatabase /> },
       { name: "MySQL", icon: <FaDatabase /> },
-      { name: "SQL Server", icon: <FaDatabase /> },
+      { name: "PostgreSQL", icon: <FaDatabase /> },
+      { name: "SQL Server (MSSQL)", icon: <FaDatabase /> },
+      { name: "Firebase", icon: <FaDatabase /> },
+   
     ],
   },
+
+  {
+    category: "Development Tools & Support",
+    skills: [
+     
+      { name: "Docker", icon: <FaServer /> },
+      { name: "Kubernetes", icon: <FaServer /> },
+      { name: "CI/CD Pipelines (Jenkins, GitHub Actions, GitLab CI/CD)", icon: <FaTools /> },
+      { name: "AWS", icon: <FaCloud /> },
+      { name: "Google Cloud", icon: <FaCloud /> },
+      { name: "Azure", icon: <FaCloud /> },
+     
+    ],
+  },
+  {
+    category: "Version Control & Collaboration",
+    skills: [
+      { name: "Git", icon: <FaCodeBranch /> },
+      { name: "GitHub", icon: <FaCodeBranch /> },
+      { name: "GitLab", icon: <FaCodeBranch /> },
+      { name: "Bitbucket", icon: <FaCodeBranch /> },
+      { name: "Agile Methodologies", icon: <FaTasks /> },
+      { name: "Scrum Framework", icon: <FaTasks /> },
+    ],
+  },
+  
+  
   {
     category: "Development Tools & Support",
     skills: [
